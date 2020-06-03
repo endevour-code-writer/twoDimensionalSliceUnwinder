fe388fe3fb795674197424facdd174ca9f62489f
ivanu <ivanudedu@gmail.com>
2020-06-03 15:53:54 +0300

    implement 2D slice composing based on input (2D slice unvind)

---
+++ internal/twoDimensionalSliceUnwinder/twoDimensionalSliceUnwinder.go
@@ -0,0 +1,62 @@
+package twoDimensionalSliceUnwinder
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+)
+
+func Unwind(args []string) []int {
+	var unwind []int
+	matrix := composeTwoDimensionalSlice(args)
+	_, unwind = unwindTwoDimensionalSlice(matrix, unwind)
+
+	return unwind
+}
+
+func composeTwoDimensionalSlice(args []string) [][]int {
+	if 0 == len(args) {
+		return createDefaultTwoDimensionalSlice()
+	}
+
+	n, err := strconv.Atoi(args[:1][0])
+
+	if err != nil {
+		fmt.Printf("The input arg %v is incorrect", n)
+		fmt.Println()
+
+		return createDefaultTwoDimensionalSlice()
+	}
+
+	matrix := make([][]int, n)
+
+	for i := 0; i < n; i++ {
+		rows := make([]int, n)
+		for index := range rows {
+			rows[index] = rand.Intn(10)
+		}
+
+		matrix[i] = rows
+	}
+
+	fmt.Printf("The matrix %v was created: ", matrix)
+	fmt.Println()
+
+	return matrix
+}
+
+func createDefaultTwoDimensionalSlice() [][]int {
+	twoDimensionalSlice := [][]int{
+			{1, 2, 3, 1},
+			{4, 5, 6, 4},
+			{7, 8, 9, 7},
+			{7, 8, 9, 7},
+	}
+
+	fmt.Println("Default 2D slice was created:")
+	fmt.Println()
+	fmt.Printf("%v", twoDimensionalSlice)
+	fmt.Println()
+
+	return twoDimensionalSlice
+}
